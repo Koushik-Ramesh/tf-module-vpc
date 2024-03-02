4e11d75a1bda35922892dea9204e2a346a64a406
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-02 18:29:06 +0530

    Autopush

--- peering.tf
+++ peering.tf
@@ -1,6 +1,6 @@
 # Creates Peering Between Default and Project Vpc
 resource "aws_vpc_peering_connection" "peer" {
-  peer_vpc_id   = aws_vpc.bar.id
+  peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.DEFAULT_VPC_ID
   auto_accept   = true
 }
