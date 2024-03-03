9a5483587283a6e920c897984dbde64e723d3e76
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-03 14:20:24 +0530

    Autopush

--- igw.tf
+++ igw.tf
@@ -6,3 +6,25 @@ resource "aws_internet_gateway" "igw" {
     Name = "roboshop-${var.ENV}-igw"
   }
 }
+
+#Creates Elastic IP Address whoch should be used by NAT Gateway, Ensure eip is provisioned first and then NAT
+resource "aws_eip" "ngw_ip" {
+  vpc   = true
+
+  tags = {
+    Name = "roboshop-${var.ENV}-ngw"
+  }
+}
+
+#Creates NAT gateway and will be attached to Public Subnet
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.example.id
+  subnet_id     = aws_subnet.public_subnet.*.id[0]
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw, aws_eip.ngw_ip]
--- route_table.tf
+++ route_table.tf
@@ -29,8 +29,14 @@ resource "aws_route_table" "private_rt" {
 
 route {
     cidr_block                  = var.DEFAULT_VPC_CIDR
+    nat_gateway_id              = aws_nat_gateway.ngw.id
+  }
+
+route {
+    cidr_block                  = "0.0.0.0/0"
     vpc_peering_connection_id   = aws_vpc_peering_connection.peer.id
   }
+
 
   tags = {
     Name = "roboshop-${var.ENV}-private-rt"
