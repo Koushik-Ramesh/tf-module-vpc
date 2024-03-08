cb674fea9ba0036d2941af683e761870edcc2272
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-08 15:08:29 +0530

    Autopush

--- outputs.tf
+++ outputs.tf
@@ -19,11 +19,11 @@ output "DEFAULT_VPC_ID" {
 }
 
 output "PUBLIC_SUBNET_IDS" {
-    value = aws_subnet.public_subnet.*.cidr_block
+    value = aws_subnet.public_subnet.*.id
 }
 
 output "PRIVATE_SUBNET_IDS" {
-    value = aws_subnet.private_subnet.*.cidr_block
+    value = aws_subnet.private_subnet.*.id
 }
 
 output "DEFAULT_VPC_CIDR" {
