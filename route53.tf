a68644b12429d091e83f13f27097fb2a978feec5
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-29 11:56:12 +0530

    Autopush

--- route53.tf
+++ route53.tf
@@ -1,5 +1,5 @@
 # Associates the VPC to the private hosted zonw
 resource "aws_route53_zone_association" "secondary" {
   zone_id = var.PRIVATE_HOSTED_ZONE_ID
-  vpc_id  = aws_vpc.secondary.id
+  vpc_id  = aws_vpc.main.id
 }
